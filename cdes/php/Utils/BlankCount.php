8dc31cf866d52dbe70a83787f5e252dc5a0f1fa7
Tianle Xu <xtl@xtlsoft.top>
2020-10-03 14:40:28 +0800

    feat(cdes): add blank counter php

--- cdes/php/Utils/BlankCount.php
+++ cdes/php/Utils/BlankCount.php
@@ -12,4 +12,93 @@ namespace Apiles\Clays\ClaysDescriptor\Utils;
 
 class BlankCount
 {
+    /**
+     * Count stack
+     *
+     * @var \SplStack
+     */
+    protected $stack = null;
+
+    /**
+     * Current level
+     *
+     * @var integer
+     */
+    protected $level = 0;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->current = 0;
+        $this->level = 0;
+    }
+
+    /**
+     * Feed a line
+     *
+     * @param string $line
+     * @return integer current level
+     */
+    public function feed(string $line): int
+    {
+        $cnt = self::countString($line);
+
+        if ($cnt > $this->stack->top()) {
+            $this->stack->push($cnt);
+            $this->level++;
+        } elseif ($cnt < $this->stack->top()) {
+            $curr = 0;
+            do {
+                if ($cnt > $curr) {
+                    throw new UnmatchedBracketsException("Blanks not matched");
+                }
+                $curr = $this->stack->pop();
+                $this->level--;
+            } while ($cnt < $curr);
+        }
+
+        return $this->getLevel();
+    }
+
+    /**
+     * Get current level
+     *
+     * @return integer
+     */
+    public function getLevel(): int
+    {
+        return $this->level;
+    }
+
+    /**
+     * Get current blank count
+     *
+     * @return integer
+     */
+    public function getCurrentCount(): int
+    {
+        return $this->stack->top();
+    }
+
+    /**
+     * Count a string
+     *
+     * @param string $str
+     * @return integer
+     */
+    public static function countString(string $str): int
+    {
+        $arr = str_split($str);
+        $k = 0;
+        $len = count($arr);
+        $cnt = 0;
+        while ($arr[$k] && $k < $len) {
+            if (!SafeExplode::isBlankCharacter($arr[$k])) break;
+            $cnt++;
+            ++$k;
+        }
+        return $cnt;
+    }
 }
--- docs/cdes/specs.md
+++ docs/cdes/specs.md
@@ -2,4 +2,13 @@
 
 `.cdes` is Clays Descriptor Format.
 
-This file format is used to describe a cluster.
+This file format is used to describe a cluster. It aims to be both human and machine readable.
+
+## 1. Header
+
+`.cdes` file starts with header:
+
+```cdes
+Clays
+
+```
